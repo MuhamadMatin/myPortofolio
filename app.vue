465471e3877dea49091cae35c79d8fb6cd1c2977
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-11-04 22:14:16 +0700

    add link in span

--- app.vue
+++ app.vue
@@ -65,9 +65,10 @@
       </section>
       <!-- addition -->
       <div class="mt-0 text-xs text-slate-400">
-        This website was created using the <NuxtLink :to="`${nuxt}`" target="blank" class="text-slate-200 duration-200 hover:text-teal-400">Nuxt</NuxtLink> framework and created using
-        <NuxtLink :to="`${tailwindcss}`" target="blank" class="text-slate-200 duration-200 hover:text-cyan-400"> Tailwind</NuxtLink>. Published using
-        <NuxtLink :to="`${netlify}`" target="blank" class="text-slate-200 duration-200 hover:bg-gradient-to-r hover:from-cyan-300 hover:to-white bg-clip-text hover:text-transparent">Netlify</NuxtLink>
+        This website was created using the <span class="text-slate-200 duration-200 hover:text-teal-400"><NuxtLink :to="`${nuxt}`" target="blank">Nuxt</NuxtLink></span> framework and created using
+        <span class="text-slate-200 duration-200 hover:text-cyan-400"><NuxtLink :to="`${tailwindcss}`" target="blank"> Tailwind</NuxtLink></span
+        >. Published using
+        <span class="text-slate-200 duration-200 hover:bg-gradient-to-r hover:from-cyan-300 hover:to-white bg-clip-text hover:text-transparent"> <NuxtLink :to="`${netlify}`" target="blank">Netlify</NuxtLink></span>
       </div>
     </div>
   </div>
