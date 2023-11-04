5eeac8a388fafe30e23748be57c8b549280c0425
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-11-04 22:17:13 +0700

    change color

--- app.vue
+++ app.vue
@@ -65,10 +65,10 @@
       </section>
       <!-- addition -->
       <div class="mt-0 text-xs text-slate-400">
-        This website was created using the <span class="text-slate-200 duration-200 hover:text-teal-400"><NuxtLink :to="`${nuxt}`" target="blank">Nuxt</NuxtLink></span> framework and created using
-        <span class="text-slate-200 duration-200 hover:text-cyan-400"><NuxtLink :to="`${tailwindcss}`" target="blank"> Tailwind</NuxtLink></span
+        This website was created using the <span class="duration-200 hover:text-teal-400"><NuxtLink :to="`${nuxt}`" target="blank">Nuxt</NuxtLink></span> framework and created using
+        <span class="duration-200 hover:text-cyan-400"><NuxtLink :to="`${tailwindcss}`" target="blank"> Tailwind</NuxtLink></span
         >. Published using
-        <span class="text-slate-200 duration-200 hover:bg-gradient-to-r hover:from-cyan-300 hover:to-white bg-clip-text hover:text-transparent"> <NuxtLink :to="`${netlify}`" target="blank">Netlify</NuxtLink></span>
+        <span class="duration-200 hover:bg-gradient-to-r hover:from-cyan-300 hover:to-white bg-clip-text hover:text-transparent"> <NuxtLink :to="`${netlify}`" target="blank">Netlify</NuxtLink></span>
       </div>
     </div>
   </div>
