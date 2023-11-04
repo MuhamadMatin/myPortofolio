e77ad4bd7680fbfec78de4197fea04351998b0d8
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2023-11-04 21:53:43 +0700

    add url in exp.name

--- app.vue
+++ app.vue
@@ -51,9 +51,9 @@
         <div class="mb-12" v-for="exp in experience" :key="exp">
           <img class="rounded-md" :src="`/images/${exp.image}`" alt="image in experience" />
           <div>
-            <p class="mt-1 mb-2 w-fit md:text-lg text-slate-200 font-semibold hover:text-teal-300 duration-200">
+            <NuxtLink :to="`${exp.URL}`" target="_blank" class="mt-1 mb-2 w-fit md:text-lg text-slate-200 font-semibold hover:text-teal-300 duration-200">
               {{ exp.name }}
-            </p>
+            </NuxtLink>
             <p class="text-xs md:text-base text-slate-400">
               {{ exp.desk }}
             </p>
@@ -80,24 +80,28 @@
       desk: 'I created a blog using the Nuxt Content from Nuxt.js as a means to learn the Nuxt module. The nuxt content is one of many modules available in Nuxt.js, a framework based on Vue.js. I can write my blog content using .md, .yml, .csv or .json. By creating this blog, I can improve my knowledge and skills about Nuxt.js and its modules',
       image: 'blog.webp',
       svg: ['nuxt.svg', 'tailwind-css.svg'],
+      URL: 'https://learnuxtblog.netlify.app/'
     },
     {
       name: 'Image Search with Unsplash API',
       desk: 'I created a website that displays various interesting photos from the Unsplash API. I used documentation provided by various sources to learn how to access and process data from the API',
       image: 'image.webp',
       svg: ['html.svg', 'css.svg', 'js.svg'],
+      URL: 'https://metallyleopart.github.io/ImageSearch/'
     },
     {
       name: 'QR Generate',
       desk: 'I created a website that can generate QR codes from text and URLs using the API from https://goqr.me/api/. I leveraged the documentation available on the website to understand how to send requests and receive responses from the API',
       image: 'qr.webp',
       svg: ['html.svg', 'css.svg', 'js.svg'],
+      URL: 'https://metallyleopart.github.io/GenerateQR/'
     },
     {
       name: 'Quiz web',
       desk: 'I created a quiz website that can test users knowledge and insight on various topics. I use web technologies such as HTML, CSS, and JavaScript to create interactive and responsive interfaces',
       image: 'quiz.webp',
       svg: ['html.svg', 'css.svg', 'js.svg'],
+      URL: 'https://metallyleopart.github.io/Question/'
     },
   ];
 </script>
