c56026ff44e8644ca3af06576e230532b3802c1e
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-11 14:31:12 +0700

    add new certificate

--- components/Achievements.vue
+++ components/Achievements.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="mb-12" v-for="achievement in achievements" :key="achievement">
-    <header class="text-white mb-0.5">{{ achievement.header }}</header>
-    <h2 class="font-bold">{{ achievement.name }}</h2>
+    <h2 class="text-white font-bold">{{ achievement.name }}</h2>
+    <h4 class="my-0.5">{{ achievement.giver }}</h4>
+    <h4 class="my-0.5">{{ achievement.header }}</h4>
+    <NuxtLink :to="`${achievement.url}`" target="blank">
+      <h5 class="hover:text-teal-500 duration-200">Credential {{ achievement.credential }}</h5>
+    </NuxtLink>
   </div>
 </template>
 
 <script setup>
   const achievements = ref([
     {
-      header: 'September 2023 — Dicoding Indonesia',
+      header: 'April 2024 — April 2027',
+      name: 'Belajar Dasar HTML',
+      credential: 'KIE3AKG',
+      giver: 'Codepolitan',
+      url: 'https://codepolitan.com/c/KIE3AKG',
+    },
+    {
+      header: 'March 2024 — March 2027',
+      name: 'Alibaba Cloud Certification',
+      credential: 'ACCD0119700100008897',
+      giver: 'Alibaba Cloud',
+      url: '',
+    },
+    {
+      header: 'September 2023 — September 2025',
       name: 'Belajar Dasar Visualisasi Data',
+      credential: 'JMZV12J5RXN9',
+      giver: 'Dicoding Indonesia',
+      url: 'https://www.dicoding.com/certificates/JMZV12J5RXN9',
     },
     {
-      header: 'August 2023 — Dicoding Indonesia',
+      header: 'August 2023 — August 2025',
       name: 'Pengenalan Ke Logika Pemrograman (Progamming Logic 101)',
+      credential: '0LZ0983QNZ65',
+      giver: 'Dicoding Indonesia',
+      url: 'https://www.dicoding.com/certificates/0LZ0983QNZ65',
     },
     {
-      header: 'July 2023 — Dicoding Indonesia',
+      header: 'July 2023 — July 2025',
       name: 'Memulai Dasar Pemrograman Untuk Menjadi Pengembang Software',
+      credential: 'MRZMQO0O3PYQ',
+      giver: 'Dicoding Indonesia',
+      url: 'https://www.dicoding.com/certificates/MRZMQO0O3PYQ',
     },
   ]);
 </script>
