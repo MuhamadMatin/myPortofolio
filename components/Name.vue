a36c9cab3eeffe0ca02368791cfad04a7dde3fde
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-11 14:42:38 +0700

    add linkedin

--- components/Name.vue
+++ components/Name.vue
@@ -3,7 +3,7 @@
     <h1 class="text-white text-4xl font-bold tracking-tight">Muhamad Matin Anugrah Pujayanto</h1>
     <h2 class="mt-3 leading-normal font-inter">
       Students with aspirations of becoming
-      <span class="hover:text-teal-300 duration-200">full stack developers</span>
+      <span class="hover:text-teal-300 duration-200">Full Stack Web Developer</span>
     </h2>
     <div class="flex gap-x-4">
       <NuxtLink v-for="link in links" :key="link" :to="`${link.URL}`" target="blank" class="mt-4">
@@ -24,6 +24,11 @@
       svg: 'instagram.svg',
       alt: 'instagram icon',
       URL: 'https://www.instagram.com/matinwithcode?igsh=MzRlODBiNWFlZA==',
+    },
+    {
+      svg: 'linkedin.svg',
+      alt: 'linkedin icon',
+      URL: 'https://linkedin.com/in/muhamad-matin-anugrah-pujayanto-b64515260',
     },
   ]);
 </script>
