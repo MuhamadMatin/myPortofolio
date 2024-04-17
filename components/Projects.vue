6255e8a587388a688e51f2b46fb3646c5d7c8d9a
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-17 18:35:09 +0700

    fix minor

--- components/Projects.vue
+++ components/Projects.vue
@@ -17,8 +17,8 @@
                 <svg class="absolute left-0 rotate-180 bottom-full h-2 w-full" x="0px" y="0px" viewBox="0 0 255 255" xml:space="preserve"><polygon points="0,0 127.5,127.5 255,0" /></svg>
               </div>
             </div> -->
-            <!-- <img class="w-6 h-6" :src="`/svg/${svg}`" alt="svg image" /> -->
-            <Icon class="w-7 h-7" :name="`${svg}`" />
+            <img class="w-6 h-6" :src="`/svg/${svg}`" alt="svg image" />
+            <!-- <Icon class="w-7 h-7" :name="`${svg}`" /> -->
           </span>
         </span>
         <NuxtLink :to="`${project.URL}`" target="blank" class="group py-2 md:py-2.5 px-4 md:px-5 bg-gray-900 text-sm duration-100 font-medium rounded-full text-gray-400 border-0 hover:text-white hover:bg-gray-700">
@@ -35,51 +35,56 @@
       name: 'Remake Image Search',
       desk: 'I rebuilt the Image Search website using the Pexel API. I used several Nuxt modules such as Nuxt Google-Font, Nuxt Security, Nuxt Image, and TailwindCSS to provide a new web experience than before. This project is part of my efforts to improve my skills in web development.',
       image: 'image-remake.webp',
-      // svg: ['Nuxt.svg', 'Tailwind-CSS.svg', 'Nuxt-Security.svg'],
-      svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
+      // svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
+      svg: ['Nuxt.svg', 'Tailwind-CSS.svg', 'Nuxt-Security.svg'],
       URL: 'https://imagenuxt.netlify.app/',
     },
     {
       name: 'Nuxt Blog',
       desk: 'I created a blog using the Nuxt Content from Nuxt.js as a means to learn the Nuxt module. The nuxt content is one of many modules available in Nuxt.js, a framework based on Vue.js. I can write my blog content using .md, .yml, .csv or .json. By creating this blog, I can improve my knowledge and skills about Nuxt.js and its modules',
       image: 'blog.webp',
-      svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
+      svg: ['Nuxt.svg', 'Tailwind-CSS.svg'],
+      // svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
       URL: 'https://blogwithnuxt.netlify.app/',
     },
     {
       name: 'Nuxt Weather',
       desk: 'I created a simple weather website I created using framwrok Nuxt',
       image: 'weather.webp',
-      svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
+      // svg: ['logos:nuxt-icon', 'logos:tailwindcss-icon'],
+      svg: ['Nuxt.svg', 'Tailwind-CSS.svg'],
       URL: 'https://weathernwithuxt.netlify.app/',
     },
     {
       name: 'Typing Game',
       desk: 'I created typing game, this game is designed to hone your finger skills and speed in an interactive way',
       image: 'typing.webp',
-      svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      // svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      svg: ['HTML.svg', 'CSS.svg', 'JavaScript.svg'],
       URL: 'https://muhamadmatin.github.io/TypingText/',
     },
     {
       name: 'Image Search With Unsplash API',
       desk: 'I created a website that displays various interesting photos from the Unsplash API. I used documentation provided by various sources to learn how to access and process data from the API',
       image: 'image.webp',
-      svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      // svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      svg: ['HTML.svg', 'CSS.svg', 'JavaScript.svg'],
       URL: 'https://muhamadmatin.github.io/ImageSearch/',
     },
     {
       name: 'Color Generate',
       desk: 'I created a color generator for inspiration and creativity and makes the color selection process easier for you.',
       image: 'color.webp',
-      svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      // svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      svg: ['HTML.svg', 'CSS.svg', 'JavaScript.svg'],
       URL: 'https://muhamadmatin.github.io/colorGenerate/',
     },
     {
       name: 'QR Generate',
       desk: 'I created a website that can generate QR codes from text and URLs using the API from https://goqr.me/api/. I leveraged the documentation available on the website to understand how to send requests and receive responses from the API',
       image: 'qr.webp',
-      svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
-      // svg: ['HTML.svg', 'CSS.svg', 'JavaScript.svg'],
+      // svg: ['vscode-icons:file-type-html', 'vscode-icons:file-type-css', 'logos:javascript'],
+      svg: ['HTML.svg', 'CSS.svg', 'JavaScript.svg'],
       URL: 'https://muhamadmatin.github.io/GenerateQR/',
     },
   ]);
