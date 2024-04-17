756350bde398c7e9728d65597b79d04e763d53d9
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-17 18:39:22 +0700

    delete nuxt-icon

--- components/Achievements.vue
+++ components/Achievements.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mb-12" v-for="achievement in achievements" :key="achievement">
     <h2 class="text-white font-bold">{{ achievement.name }}</h2>
-    <h4 class="my-0.5">{{ achievement.giver }}</h4>
-    <h4 class="my-0.5">{{ achievement.header }}</h4>
+    <p class="my-0.5">{{ achievement.giver }}</p>
+    <p class="my-0.5">{{ achievement.header }}</p>
     <NuxtLink :to="`${achievement.url}`" target="blank">
       <h5 class="hover:text-teal-500 duration-200">Credential {{ achievement.credential }}</h5>
     </NuxtLink>
