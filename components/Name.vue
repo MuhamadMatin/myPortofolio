cb45e942c35a040718581b1d33da47ec5c761041
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-04-21 23:00:05 +0700

    add quote block

--- components/Name.vue
+++ components/Name.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <h1 class="text-white text-4xl font-bold tracking-tight">Muhamad Matin Anugrah Pujayanto</h1>
-    <h2 class="mt-3 leading-normal font-inter">
-      Student with aspirations of becoming
-      <span class="hover:text-teal-300 duration-200">Full Stack Web Developer</span>
-    </h2>
+    <blockquote class="px-4 py-2.5 my-4 border-s-4 border-[#fff] max-w-sm lg:max-w-md">
+      <h2 class="italic leading-relaxe">
+        "Student with aspirations of becoming
+        <span class="hover:text-teal-300 duration-200 cursor-pointer">Full Stack Web Developer</span>."
+      </h2>
+    </blockquote>
     <div class="flex gap-x-4">
       <NuxtLink v-for="link in links" :key="link" :to="`${link.URL}`" target="blank" area-label="connect withh me🤝">
         <!-- <Icon class="mt-2 h-10 w-10 text-[#24292F] active:text-white hover:text-white duration-200" :name="`${link.svg}`" /> -->
-        <NuxtImg class="mt-2 w-10 h-10 active:text-white hover:text-white duration-200" :src="`/svg/${link.svg}`" :alt="`${link.alt}`" />
+        <NuxtImg class="w-10 h-10 active:text-white hover:text-white duration-200" :src="`/svg/${link.svg}`" :alt="`${link.alt}`" />
       </NuxtLink>
     </div>
   </div>
