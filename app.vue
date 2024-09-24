e33a7aaf6e81a1fa1d2f53a188a560882491f44a
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-09-24 21:21:27 +0700

    add seo and image meta

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import type { Title } from '#build/components';
+  const route = useRequestURL().origin;
 </script>
 
 <template>
@@ -7,12 +7,13 @@
     <!-- ini merupakan cara menambahkan head pada Nuxt -->
     <Html lang="en" />
     <!-- Google verification -->
-    <Title>Muhamad Matin Portfolio</Title>
+    <Title>Matin's Portfolio</Title>
     <Meta name="google-site-verification" content="ncQbs7NO4o7Kqbi52iJFLJwFkgho9AW43lM5F4YH694" />
 
     <!-- Meta umum untuk SEO -->
+    <meta name="title" content="Muhamad Matin Portofolio" />
     <Meta name="description" content="Explore Matin's portfolio. This is the first portfolio website created using Nuxt.js, showcasing skills in web development." />
-    <Meta name="keywords" content="portfolio matin,Matin Portfolio, Nuxt.js portfolio, Muhamad Matin, muhamad matin, web development, full stack developer, frontend, backend" />
+    <Meta name="keywords" content="matin, muhamad matin, Muhamad Matin, portofolio, Muhammad Matin, muhammad matin, portfolio matin, Matin Portfolio, Nuxt.js portfolio, web development, full stack developer, frontend, backend" />
     <Meta name="author" content="Matin" />
     <!-- Meta untuk mobile responsiveness -->
     <Meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -20,15 +21,15 @@
     <!-- Open Graph Meta untuk Facebook, LinkedIn, dll. -->
     <Meta property="og:title" content="Matin's Portfolio" />
     <Meta property="og:description" content="Explore Matin's portfolio. This is the first portfolio website created using Nuxt.js, showcasing skills in web development." />
-    <Meta property="og:image" content="https://example.com/portfolio-image.png" />
+    <Meta property="og:image" :content="`${route}/images/thumbnail.webp`" />
     <Meta property="og:type" content="website" />
-    <Meta property="og:url" content="https://www.muhamadmatin.my.id" />
+    <Meta property="og:url" :content="route" />
 
     <!-- Meta untuk Twitter -->
     <Meta name="twitter:card" content="summary_large_image" />
     <Meta name="twitter:title" content="Matin's Portfolio" />
     <Meta name="twitter:description" content="Explore Matin's portfolio. This is the first portfolio website created using Nuxt.js, showcasing skills in web development." />
-    <Meta name="twitter:image" content="https://example.com/portfolio-image.png" />
+    <Meta name="twitter:image" :content="`${route}/images/thumbnail.webp`" />
   </Head>
   <NuxtPage />
 </template>
