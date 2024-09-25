76df0b833999788a636819254e10e94d1c779935
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2024-09-25 14:35:32 +0700

    edit seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon', '@nuxtjs/sitemap'],
-
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon', '@nuxtjs/sitemap', '@nuxtjs/seo'],
   ssr: true,
 
   site: {
     url: 'https://www.muhamadmatin.my.id',
     name: 'muhamad matin',
+    description: "Explore Matin's portfolio. This is the first portfolio website created using Nuxt.js, showcasing skills in web development.",
+    defaultLocale: 'en',
   },
+
+  compatibilityDate: '2024-09-25',
 });
