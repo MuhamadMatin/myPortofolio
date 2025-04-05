a5f1b6b710eb2efcb8be253027e23812423d16ab
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2025-04-05 14:32:51 +0700

    fix screen on tablet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-5 pt-10 mx-auto md:pb-5 md:max-w-screen-md lg:p-0 lg:px-5 lg:grid lg:grid-cols-2 lg:max-w-screen-xl lg:h-screen">
+  <div class="px-5 pt-10 mx-auto md:pb-5 md:max-w-md lg:p-0 lg:px-5 lg:grid lg:grid-cols-2 lg:max-w-screen-xl lg:h-screen">
     <slot />
   </div>
 </template>
